133e736af109a079ad1597bb574304a143d818bf
Kat Maldon <katharinamaldon@gmail.com>
2019-06-27 18:20:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,9 @@ class App < Sinatra::Base
   end
 
   get '/friends' do
-   
+   @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+  erb :friends
 
   end
 end
